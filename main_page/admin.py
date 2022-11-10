94c4d66565f940ce960da354c3ee27c07897ab8a
Halyna Svoboda <svoboda006@gmail.com>
2022-11-10 16:49:34 +0200

    Create chefs.html and changed index.html

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Dish, Events, Presentation, Gallery
+from .models import Category, Dish, Events, Presentation, Gallery, Information, Testimonials, Chefs
 
 # Register your models here.
 admin.site.register(Category)
@@ -7,5 +7,8 @@ admin.site.register(Dish)
 admin.site.register(Events)
 admin.site.register(Presentation)
 admin.site.register(Gallery)
+admin.site.register(Information)
+admin.site.register(Testimonials)
+admin.site.register(Chefs)
 
 
--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Category, Dish, Presentation, Events, Gallery
+from .models import Category, Dish, Presentation, Events, Gallery, Information, Testimonials, Chefs
 import random
 
 # Create your views here.
@@ -8,10 +8,12 @@ def main_view(request):
     dishes = Dish.objects.filter(is_visible=True)
     categories = Category.objects.filter(is_visible=True)
     specials = Dish.objects.filter(is_visible=True, is_special=True)
-    about = Presentation.objects.filter(is_visible=True, why_us=False)
+    about = Presentation.objects.filter(is_visible=True, about=True)
     why_us = Presentation.objects.filter(is_visible=True, why_us=True)
     events = Events.objects.filter(is_visible=True)
     photos = Gallery.objects.all().order_by('?')
+    greetings = Presentation.objects.filter(is_visible=True, greetings=True)
+    chefs = Chefs.objects.filter(is_visible=True)
 
 
     return render(request, 'base.html', context={
@@ -22,5 +24,7 @@ def main_view(request):
         'why_us': why_us,
         'events': events,
         'photos': photos,
+        'greetings': greetings,
+        'chefs': chefs,
 
     })
